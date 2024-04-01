a32a399b61a4d2548ff31efbc2fbf45c83f2176f
ziad <zemad3040@gmail.com>
2024-04-01 18:26:48 +0200

    EDIT

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ require('dotenv').config();
 app.use(express.json({ limit: '50mb' }));
 const specs = swaggerJsdoc(require('./swagger.json'));
 
-const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
-
+const CSS_URL =
+    "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+    
 app.use('/docs', swagger.serve, swagger.setup(specs, {
     explorer: false,
     customSiteTitle: 'Sticks Docs',
